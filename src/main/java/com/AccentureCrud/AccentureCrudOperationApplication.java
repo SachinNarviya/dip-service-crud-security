eab5b064d69c5d0c112b6e5aaf49671fc53eb29c
SachinNarviya <sachinnarvariya209@gmail.com>
2024-10-19 21:42:55 +0530

    intial comit on master branch

---
+++ src/main/java/com/AccentureCrud/AccentureCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.AccentureCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentureCrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentureCrudOperationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AccentureCrud/AccentureCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AccentureCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentureCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
