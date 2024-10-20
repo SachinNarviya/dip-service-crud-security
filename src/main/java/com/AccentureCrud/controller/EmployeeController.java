d9aa37e0e7b739792b061515643ba931f6d00a76
SachinNarviya <sachinnarvariya209@gmail.com>
2024-10-20 18:50:02 +0530

    Handled global exception so don't need to handle the exceptions in controller class

--- src/main/java/com/AccentureCrud/controller/EmployeeController.java
+++ src/main/java/com/AccentureCrud/controller/EmployeeController.java
@@ -1,22 +1,11 @@
 package com.AccentureCrud.controller;
 
 import com.AccentureCrud.entity.Employee;
-import com.AccentureCrud.exception.BusinessServiceException;
-import com.AccentureCrud.exception.ControllerException;
 import com.AccentureCrud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/employee")
@@ -26,62 +15,25 @@ public class EmployeeController {
 
     @GetMapping("/getAllEmployee")
     public ResponseEntity<?> getAllEmployee() {
-        try {
-            var allEmployee = employeeService.getAllEmployee();
-            return new ResponseEntity<>(allEmployee, HttpStatus.OK);
-        } catch (BusinessServiceException e) {
-            var controllerException = new ControllerException(e.getErrorCode(), e.getErrorMessage());
-            return new ResponseEntity<>(controllerException, HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
-            var controllerException = new ControllerException("608",
-                    "Something went wrong in controller while fetching the employee list" + e.getMessage());
-            return new ResponseEntity<>(controllerException, HttpStatus.BAD_REQUEST);
-        }
+        var allEmployee = employeeService.getAllEmployee();
+        return new ResponseEntity<>(allEmployee, HttpStatus.OK);
     }
 
     @GetMapping("/getEmployeeById/{id}")
     public ResponseEntity<?> getEmployeeById(@PathVariable Long id) {
-        try {
-            var employee = employeeService.getEmployeeById(id);
-            return new ResponseEntity<>(employee, HttpStatus.OK);
-        } catch (BusinessServiceException e) {
-            var controllerException = new ControllerException(e.getErrorCode(), e.getErrorMessage());
-            return new ResponseEntity<>(controllerException, HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
-            var controllerException = new ControllerException("609",
-                    "Something went wrong in controller while fetching the employee object" + e.getMessage());
-            return new ResponseEntity<>(controllerException, HttpStatus.BAD_REQUEST);
-        }
+        var employee = employeeService.getEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
     @PostMapping("/addEmployee")
     public ResponseEntity<?> addEmployee(@RequestBody Employee employee) {
-        try {
-            var savedEmp = employeeService.addEmployee(employee);
-            return new ResponseEntity<>(savedEmp, HttpStatus.CREATED);
-        } catch (BusinessServiceException e) {
-            var controllerException = new ControllerException(e.getErrorCode(), e.getErrorMessage());
-            return new ResponseEntity<>(controllerException, HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
-            var controllerException = new ControllerException("610",
-                    "Something went wrong in controller while saving the employee object" + e.getMessage());
-            return new ResponseEntity<>(controllerException, HttpStatus.BAD_REQUEST);
-        }
-
+        var savedEmp = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(savedEmp, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/deleteEmployee/{id}")
     public ResponseEntity<?> deleteEmployeeById(@PathVariable Long id) {
-        try {
-            employeeService.deleteEmployee(id);
-            return new ResponseEntity<>(HttpStatus.ACCEPTED);
-        } catch (BusinessServiceException e) {
-            var controllerException = new ControllerException(e.getErrorCode(), e.getErrorMessage());
-            return new ResponseEntity<>(controllerException, HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
-            var controllerException = new ControllerException("611",
-                    "Something went wrong in controller while deleting the employee object" + e.getMessage());
-            return new ResponseEntity<>(controllerException, HttpStatus.BAD_REQUEST);
-        }
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
